4b870ade9a8807898a1daeb73396328fe323151c
Malo Hervoche <mhervoch@3G3.42angouleme.fr>
2024-03-19 20:09:15 +0100

    firat redir

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,12 +6,31 @@
 /*   By: lboiteux <lboiteux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 23:14:00 by mhervoch          #+#    #+#             */
-/*   Updated: 2024/03/16 01:32:36 by lboiteux         ###   ########.fr       */
+/*   Updated: 2024/03/19 19:55:55 by mhervoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
+int	get_bool(t_ms *ms, int *bool_in, int *bool_out)
+{
+	t_list	tmp;
+
+	tmp = lst;
+	while (ms->tmp)
+	{
+		if (!ft_strncmp(ms->tmp->content, ">", 1))
+			*bool_out++;
+		if (!ft_strncmp(ms->tmp->content, "<", 1))
+			*bool_in++;
+	}
+	return (1);
+}
+
 void	redirection(t_ms *ms)
 {
+	int	bool_in;
+	int	bool_out;
+
+	get_bool(ms, &bool_in, &bool_out);
 }
