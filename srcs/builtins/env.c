4575cac9471b279cd0350420498c5b5c65788434
lboiteux <loevanbtx.pro@gmail.com>
2024-04-02 20:35:58 +0200

    Update env, working 100%

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: lboiteux <lboiteux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 01:40:35 by mhervoch          #+#    #+#             */
-/*   Updated: 2024/03/16 01:32:36 by lboiteux         ###   ########.fr       */
+/*   Updated: 2024/04/02 20:24:55 by lboiteux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,25 @@ void	env(t_ms *ms)
 	char	*str;
 
 	i = 0;
-	while (ms->env[i])
+	if (!ms->lst->next)
 	{
-		str = ft_strjoin(ms->env[i], "\n", NULL, 0b000);
-		ft_dprintf(1, str);
-		free(str);
-		i++;
+		while (ms->env[i])
+		{
+			str = ft_strjoin(ms->env[i], "\n", NULL, 0b000);
+			ft_dprintf(1, str);
+			free(str);
+			i++;
+		}
+	}
+	if (ms->lst->next->content[0] == '-')
+	{
+		g_exit = 125;
+		ft_dprintf(2, "env: invalid option -- %c\n", ms->lst->next->content[1]);
+	}
+	else
+	{
+		g_exit = 127;
+		ft_dprintf(2, "env: %s: No such file or directory\n", \
+		ms->lst->next->content);
 	}
 }
