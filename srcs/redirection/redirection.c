08cc0db2e4b0ffb45766ed800c73e89018e66106
Malo Hervoche <mhervoch@1F1.42angouleme.fr>
2024-04-16 17:52:52 +0200

    v4 redir exec

--- srcs/redirection/redirection.c
+++ srcs/redirection/redirection.c
@@ -6,7 +6,7 @@
 /*   By: lboiteux <lboiteux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 23:14:00 by mhervoch          #+#    #+#             */
-/*   Updated: 2024/04/16 17:10:21 by mhervoch         ###   ########.fr       */
+/*   Updated: 2024/04/16 17:50:49 by mhervoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	redirection(t_redirlst *redir, int fd_out)
 	{
 		if (redir->type == REDIR_IN)
 		{
-			if (!check_file(redir->file) && !redir->next)
+			if (!check_file(redir->file))
 				return (0);
 			fd_in = open(redir->file, O_RDONLY);
 			if (redir->next)
@@ -60,6 +60,8 @@ int	redirection(t_redirlst *redir, int fd_out)
 					close(fd_out);
 			}
 		}
+		if (redir->type == APPEND)
+			append(redir, fd_out);
 		redir = redir->next;
 	}
 	if (fd_in)
@@ -73,11 +75,23 @@ int	redirection(t_redirlst *redir, int fd_out)
 	return (1);
 }
 
-int	append(t_cmdlist *cmdlist, int fd_out)
+int	here_doc(t_cmdlist *cmdlst)
 {
-	check_file(cmdlist->redir->file);
-	fd_out = open(cmdlist->redir->file, O_APPEND | O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	dup2(fd_out, STDOUT_FILENO);
+	char *line;
+
+	while (!strncmp(cmdlst->redir->file, line, ft_strlen(cmdlst->redir->file)))
+	{
+		line = readline(">");
+	}
+}
+
+int	append(t_redirlst *redir, int fd_out)
+{
+	fd_out = open(redir->file, O_APPEND | O_WRONLY | O_CREAT, 0644);
+	if (!check_outfile(redir->file, fd_out))
+		return (0);
+	if (fd_out)
+		dup2(fd_out, STDOUT_FILENO);
 	if (fd_out > 2)
 		close(fd_out);
 	return (1);
@@ -92,7 +106,7 @@ int	display(t_cmdlist *cmdlst, int fd_out)
 	}
 	if (cmdlst->redir->type == APPEND)
 	{
-		if (!append(cmdlst, fd_out))
+		if (!append(cmdlst->redir, fd_out))
 			return (0);
 	}
 	return (1);
